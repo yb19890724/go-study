e73176a7ccca6c8b312e749e6e3a61d606f798c2
hyb <354037968@qq.com>
2019-01-19 23:53:27 +0800

    更改错误命令和代码格式

--- fileroutine/src/test.go
+++ fileroutine/src/test.go
@@ -1,4 +1,4 @@
-package src
+package main
 
 import (
 	"bufio"
@@ -11,8 +11,8 @@ import (
 type LogProcess struct {
 	readChannel  chan []byte //读取管道
 	writeChannel chan string //写入管道
-	read         Reader		 //定义接收读接口
-	write        Writer		 //定义接收写接口
+	read         Reader      //定义接收读接口
+	write        Writer      //定义接收写接口
 }
 
 type Reader interface {
@@ -34,25 +34,25 @@ type WriteFile struct {
 //读取模块
 func (r *ReadFile) Read(readChannel chan []byte) {
 
-	f,err :=os.Open(r.path)
+	f, err := os.Open(r.path)
 
-	if nil !=err { //打开文件是否正常
-		panic(fmt.Sprintf("oepn file errpr %s",err.Error()))
+	if nil != err { //打开文件是否正常
+		panic(fmt.Sprintf("oepn file errpr %s", err.Error()))
 	}
 
 	//从文件末尾开始逐行读取文件内容 (读取最新的内容)
-	f.Seek(0,2)
-	read :=bufio.NewReader(f)
+	f.Seek(0, 2)
+	read := bufio.NewReader(f)
 
-	for   {
+	for {
 
-		line , err := read.ReadBytes('\n')
+		line, err := read.ReadBytes('\n')
 
 		if err == io.EOF {
 			time.Sleep(500 * time.Millisecond)
 			continue
-		} else if err !=nil {
-			panic(fmt.Sprintf("ReadBytes error:%s",err.Error()))
+		} else if err != nil {
+			panic(fmt.Sprintf("ReadBytes error:%s", err.Error()))
 		}
 
 		readChannel <- line[:len(line)-1]
@@ -63,7 +63,7 @@ func (r *ReadFile) Read(readChannel chan []byte) {
 //解析模块
 func (l *LogProcess) Process() {
 
-	for  v :=range l.readChannel{
+	for v := range l.readChannel {
 		l.writeChannel <- string(v)
 	}
 
@@ -75,33 +75,35 @@ func (l *LogProcess) Process() {
 func (w *WriteFile) Write(writeChannel chan string) {
 	//循环读取
 
-	for  v :=range writeChannel{
-		fmt.Println(fmt.Sprintf("%s",v))
+	for v := range writeChannel {
+		fmt.Println(fmt.Sprintf("%s", v))
 	}
 }
 
-
-
 func main() {
 
+	fmt.Print(1)
+
+
 	read := &ReadFile{
-		path: "D:\\phpStudy\\PHPTutorial\\nginx\\logs\\access.log",
+		path: "/Users/huangyibing/Home/access.log",
 	}
 
 	write := &WriteFile{
 		path: "D:\\phpStudy\\PHPTutorial\\nginx\\logs\access.log",
 	}
 
+
 	lp := &LogProcess{
 		readChannel:  make(chan []byte),
 		writeChannel: make(chan string),
-		read:read,
-		write:write,
+		read:         read,
+		write:        write,
 	}
 
 	go lp.read.Read(lp.readChannel)
 	go lp.Process()
 	go lp.write.Write(lp.writeChannel)
 
-	time.Sleep(30*time.Second)
+	time.Sleep(30 * time.Second)
 }
